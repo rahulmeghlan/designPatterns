b07dc4e2aa405081db1ec9b95b132b190b396290
Rahul Meghlan <rahulmeghlan@gmail.com>
2014-05-12 11:31:12 +0530

    more examples related to the constructor functionality in js

--- inheritance/index.js
+++ inheritance/index.js
@@ -16,3 +16,40 @@ console.log(iAmExtened instanceof  iWillExtend) // true
 console.log(iAmExtened instanceof  myConstructor) // true
 
 // Thus the above idea shows how iAmExtended got the properties of myConstructor + iWillExtend
+
+
+// Calling constructor of an object
+
+var myInstance = new myConstructor();
+var a = new myInstance.constructor();
+
+console.log(a.constructor === myConstructor) // true
+
+
+// ========== Getter and Setter Functions provided by the browser ========
+
+/* todo : This need much better understanding, this is not clear as of now
+function Person(nameStr){
+    var name = nameStr;
+
+    this.__defineGetter__("name", function(){
+        return name;
+    });
+
+    this.__defineSetter__("name", function(nameStr){
+        name = nameStr;
+    });
+}
+
+Person.prototype = {
+    get name(){
+        return this._name;
+    },
+    set name(nameStr){
+        this._name = nameStr;
+    }
+};
+*/
+
+
+
